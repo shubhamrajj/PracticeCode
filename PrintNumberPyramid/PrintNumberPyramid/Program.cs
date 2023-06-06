5eeb5bbc3e91a99411c7b188a3ff2d310aeb0245
Shubham Raj <76194714+shubhamrajj@users.noreply.github.com>
2023-06-06 09:36:06 +0530

    Add files via upload

---
+++ PrintNumberPyramid/PrintNumberPyramid/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 5;
+
+        PrintNumberPyramid(rows);
+    }
+
+    static void PrintNumberPyramid(int rows)
+    {
+        int number = 1;
+        int spaces = rows - 1;
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= spaces; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write(number + " ");
+                number++;
+            }
+
+            Console.WriteLine();
+            spaces--;
+        }
+    }
+}
---
+++ PrintPyramid/PrintPyramid/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int pyramidSize = 5;
+        char baseChar = 'A';
+
+        PrintPyramid(pyramidSize, baseChar);
+    }
+
+    static void PrintPyramid(int size, char baseChar)
+    {
+        int totalChars = (2 * size) - 1;
+        int midpoint = totalChars / 2;
+        int charValue = (int)baseChar;
+
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < totalChars; col++)
+            {
+                if (col >= midpoint - row && col <= midpoint + row)
+                {
+                    Console.Write((char)charValue);
+                    charValue++;
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
