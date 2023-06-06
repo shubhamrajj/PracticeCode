6f48af9574736fbd6803f5270d63928185633e11
Shubham Raj <76194714+shubhamrajj@users.noreply.github.com>
2023-06-06 09:00:09 +0530

    Add files via upload

---
+++ Highest Pair/Highest Pair/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 10, 7, 45, 23, 56, 12, 98, 34 };
+        int[] greatestPair = FindGreatestPair(array);
+
+        Console.WriteLine("The greatest pair is: {0} and {1}", greatestPair[0], greatestPair[1]);
+    }
+
+    static int[] FindGreatestPair(int[] array)
+    {
+        if (array.Length < 2)
+        {
+            throw new ArgumentException("Array must contain at least 2 elements");
+        }
+
+        int[] greatestPair = { array[0], array[1] };
+
+        for (int i = 2; i < array.Length; i++)
+        {
+            if (array[i] > greatestPair[0])
+            {
+                greatestPair[1] = greatestPair[0];
+                greatestPair[0] = array[i];
+            }
+            else if (array[i] > greatestPair[1])
+            {
+                greatestPair[1] = array[i];
+            }
+        }
+
+        return greatestPair;
+    }
+}
