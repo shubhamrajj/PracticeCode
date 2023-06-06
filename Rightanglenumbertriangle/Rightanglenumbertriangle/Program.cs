c50d45deb14a4bf2fe915b7ea30602c03a6c0c39
Shubham Raj <76194714+shubhamrajj@users.noreply.github.com>
2023-06-06 09:01:07 +0530

    Add files via upload

---
+++ Rightanglenumbertriangle/Rightanglenumbertriangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 5; // Number of rows in the triangle
+
+        // Loop for each row
+        for (int i = 1; i <= rows; i++)
+        {
+            // Loop to print the numbers in each row
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+
+            Console.WriteLine(); // Move to the next line after each row
+        }
+    }
+}
